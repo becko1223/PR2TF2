8907b97902573cd2b281a5098d1cd18b6aa8e8fd
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-08-12 18:01:52 +0900

    lstm option for sequence

--- PR2/Ray_ACNet.py
+++ PR2/Ray_ACNet.py
@@ -67,7 +67,7 @@ class ACNet(tf.keras.Model):
 
         self.h3=layers.ReLU()
 
-        self.lstm=layers.LSTM(units=RNN_SIZE,return_state=True)
+        self.lstm=layers.LSTM(units=RNN_SIZE,return_state=True,return_sequences=True)
 
         self.h0=tf.zeros((1,RNN_SIZE))
         self.c0=tf.zeros((1,RNN_SIZE))
