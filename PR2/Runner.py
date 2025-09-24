da3e71a11c707aa7f08a17e65da089f8eb7d6257
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-09-24 13:20:35 +0900

    cpu

--- PR2/Runner.py
+++ PR2/Runner.py
@@ -252,13 +252,13 @@ class Runner(object):
 import multiprocessing
 cpu=multiprocessing.cpu_count()
 
-@ray.remote(num_cpus=(cpu*3)//19, num_gpus= 1.0 / (NUM_META_AGENTS - NUM_IL_META_AGENTS + 1))
+@ray.remote(num_cpus=max([(cpu*3)//19,1]), num_gpus= 1.0 / (NUM_META_AGENTS - NUM_IL_META_AGENTS + 1))
 class RLRunner(Runner):
     def __init__(self, metaAgentID):        
         super().__init__(metaAgentID)
 
 
-@ray.remote(num_cpus=(cpu*1)//19, num_gpus=0)
+@ray.remote(num_cpus=max([(cpu*1)//19,1]), num_gpus=0)
 class imitationRunner(Runner):
     def __init__(self, metaAgentID):        
         super().__init__(metaAgentID)
