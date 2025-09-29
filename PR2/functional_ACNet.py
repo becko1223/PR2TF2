7366ffd73eebd66676a3d66067708ea6ee6d36e8
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-09-29 15:58:02 +0900

    functional

--- PR2/functional_ACNet.py
+++ PR2/functional_ACNet.py
@@ -64,7 +64,7 @@ def createmodel():
 
 
     #y = layers.Lambda(lambda t: tf.reshape(t,[-1,3]))(goal_inputs)
-    y = layers.Dense(units=GOAL_REPR_SIZE, activation='relu', name='goal_dense')(y)
+    y = layers.Dense(units=GOAL_REPR_SIZE, activation='relu', name='goal_dense')(goal_inputs)
 
 
     x_combined = layers.Concatenate(axis=-1)([x, y])
