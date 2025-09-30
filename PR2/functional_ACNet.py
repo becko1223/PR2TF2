644761d59840232e23ec16bfffef100d772e9014
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-09-30 17:03:40 +0900

    functional

--- PR2/functional_ACNet.py
+++ PR2/functional_ACNet.py
@@ -59,7 +59,9 @@ def createmodel():
     x=layers.TimeDistributed(layers.Conv2D(filters=RNN_SIZE // 4,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu'))(x)
     x=layers.TimeDistributed(layers.MaxPool2D(2))(x)
 
-    x=layers.TimeDistributed(layers.Conv2D(filters=RNN_SIZE - GOAL_REPR_SIZE,kernel_size=2,strides=1,padding="valid",data_format="channels_last",kernel_initializer=w_init, activation=None))(x)
+    x=layers.TimeDistributed(layers.MaxPool2D(2))(x)
+
+    x=layers.TimeDistributed(layers.Conv2D(filters=RNN_SIZE - GOAL_REPR_SIZE,kernel_size=1,strides=1,padding="valid",data_format="channels_last",kernel_initializer=w_init, activation=None))(x)
 
     x = layers.TimeDistributed(layers.Flatten())(x) 
     x = layers.TimeDistributed(layers.ReLU())(x)
