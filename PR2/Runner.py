9b9c3d5cf32432afc18869b35c3de440b0e9101f
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-10-11 15:02:03 +0900

    agent to 2

--- PR2/Runner.py
+++ PR2/Runner.py
@@ -278,7 +278,7 @@ class RLRunner(Runner):
         super().__init__(metaAgentID)
 
 
-@ray.remote(num_cpus=max([(cpu*1)//19,1]), num_gpus=0)
+@ray.remote(num_cpus=max([(cpu*1)//19,1]), num_gpus=0,memory=4_000_000_000)
 class imitationRunner(Runner):
     def __init__(self, metaAgentID):        
         super().__init__(metaAgentID)
