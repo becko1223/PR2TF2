0c1ded67fcbf49e9207f7699698779100fd29914
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-10-12 18:22:36 +0900

    imi memory

--- PR2/Worker.py
+++ PR2/Worker.py
@@ -61,16 +61,6 @@ class Worker():
     
 
 
-        self.wrapped_local_AC = tf.function(
-            lambda obs, goal, h, c: self.local_AC([obs, goal, h, c]),
-            # tf.function のトレースが毎回再実行されないように、入力の型と形状を明示
-            input_signature=[
-                tf.TensorSpec(shape=[1, None, 11, 11, 11], dtype=tf.float32),  # obs (B, S, D, H, W)
-                tf.TensorSpec(shape=[1, None, 3], dtype=tf.float32),          # goal (B, S, F)
-                tf.TensorSpec(shape=[1, 512], dtype=tf.float32),              # h_state (B, RNN_SIZE)
-                tf.TensorSpec(shape=[1, 512], dtype=tf.float32),              # c_state (B, RNN_SIZE)
-            ]
-        )
         
 
         self.learningAgent=learningAgent
