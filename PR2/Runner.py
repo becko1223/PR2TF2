c7a87fca3970ce6c2b5f5add5f70cf13634116e0
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-10-12 14:36:18 +0900

    imi memory to 8

--- PR2/Runner.py
+++ PR2/Runner.py
@@ -278,7 +278,7 @@ class RLRunner(Runner):
         super().__init__(metaAgentID)
 
 
-@ray.remote(num_cpus=1, num_gpus=0,memory=4_000_000_000)
+@ray.remote(num_cpus=1, num_gpus=0,memory=8_000_000_000)
 class imitationRunner(Runner):
     def __init__(self, metaAgentID):        
         super().__init__(metaAgentID)
