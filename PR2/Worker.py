5d24a115666b59c1e39ab784abddda50701ac303
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-10-13 21:07:11 +0900

    imi memory

--- PR2/Worker.py
+++ PR2/Worker.py
@@ -314,16 +314,18 @@ class Worker():
        
         print("imi worker")
         self.env._reset()
-        
-        rollouts, targets_done = self.parse_path(episode_count)
 
-         #実験的にこの位置でモデル動かしたら動いてくれる。ここより後だとメモリエラーが出る。
+         #実験的にこの位置でモデル動かしたら動いてくれる。parse_pathより後だとメモリエラーが出る。
         dummy_obs=tf.zeros((1,1,11,11,11))
         dummy_goals=tf.zeros((1,1,3))
         dummy_hstate=tf.zeros((1,512))
         dummy_cstate=(tf.zeros((1,512)))
         policy,_,_,h_states,c_states=self.wrapped_local_AC(dummy_obs,dummy_goals,dummy_hstate,dummy_cstate)
         print("dummy_policy",policy)
+        
+        rollouts, targets_done = self.parse_path(episode_count)
+
+        
 
 
         
