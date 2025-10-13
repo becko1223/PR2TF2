86aa25a6125c55e8ee94388bc1e0f3757a91bbc0
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-10-13 18:19:43 +0900

    imi memory

--- PR2/Worker.py
+++ PR2/Worker.py
@@ -190,12 +190,12 @@ class Worker():
                 
                 obs=tf.transpose(obs,[0,1,3,4,2])
                 
-                print("model calc")
+                print("before model calc")
                 policy,_,_,h_states,c_states=self.local_AC([obs,goals,h_state,c_state])
 
               
                 optimal_actions_onehot = tf.one_hot(rollout_actions, a_size, dtype=tf.float32)
-                print("loss calc")
+                print("before loss calc")
                 total_loss=tf.reduce_mean(tf.keras.backend.categorical_crossentropy(optimal_actions_onehot, policy))
 
                 i_grads = tape.gradient(total_loss,self.local_AC.trainable_variables)
