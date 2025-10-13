e0f2e2d5d9cc3e91b860bfb72dea6c7e7764249d
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-10-13 20:02:41 +0900

    imi memory

--- PR2/Worker.py
+++ PR2/Worker.py
@@ -84,7 +84,12 @@ class Worker():
 
 
     def calculateImitationGradient(self, rollout, episode_count):
-        
+        dummy_obs=tf.zeros((1,1,11,11,11))
+        dummy_goals=tf.zeros((1,1,3))
+        dummy_hstate=tf.zeros((1,512))
+        dummy_cstate=(tf.zeros((1,512)))
+        policy,_,_,h_states,c_states=self.wrapped_local_AC(dummy_obs,dummy_goals,dummy_hstate,dummy_cstate)
+
         rollout = np.array(rollout, dtype=object)
         print("rollout shape", rollout.shape)
         # we calculate the loss differently for imitation
@@ -204,7 +209,9 @@ class Worker():
                     print("rollout_obs[i].shape before expand:", rollout_obs[i].shape)
                     dummy_obs=tf.zeros((1,1,11,11,11))
                     dummy_goals=tf.zeros((1,1,3))
-                    policy,_,_,h_states,c_states=self.wrapped_local_AC(dummy_obs,dummy_goals,h_state,c_state)
+                    dummy_hstate=tf.zeros((1,512))
+                    dummy_cstate=(tf.zeros((1,512)))
+                    policy,_,_,h_states,c_states=self.wrapped_local_AC(dummy_obs,dummy_goals,dummy_hstate,dummy_cstate)
                     print("dummy policy",policy)
                     time.sleep(3)
                     policy,_,_,h_states,c_states=self.wrapped_local_AC(obs,goals,h_state,c_state)
