c00fc5ee6250859e2007c4493cd1f16e5c05a06f
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-10-17 15:10:03 +0900

    TDMPC

--- PR2/Worker.py
+++ PR2/Worker.py
@@ -212,7 +212,9 @@ class Worker():
         last_policies=tf.nn.softmax(last_policies)
         
         last_actions=tf.map_fn(lambda last_policy: tf.random.categorical(tf.math.log(last_policy), num_samples=1),last_policies,dtype=tf.int64)  #ここは一個サンプリングよりも、前行動について確率重み付け平均撮った方が良いのかも。でも連続空間でやってる先行コードはこっち。
+        last_actions=tf.squeeze(last_actions,1)
         last_actions=tf.one_hot(last_actions,a_size)
+        print("last_actions shape:",last_actions.shape)
         q1_value=self.local_ACRD.q1(current_latents,last_actions)
         q2_value=self.local_ACRD.q2(current_latents,last_actions)
         q_value=tf.minimum(q1_value,q2_value)
