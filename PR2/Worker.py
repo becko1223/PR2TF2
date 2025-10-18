f0b40010faeb89dd35dda75b0521e2d14a1d7b28
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-10-18 17:41:58 +0900

    TDMPC

--- PR2/Worker.py
+++ PR2/Worker.py
@@ -289,8 +289,7 @@ class Worker():
         print("score shape:",score.shape)
         print("elite_coord shape:",elite_coord.shape)
         print("mean coord shape",mean_coord.shape)
-        std=tf.sqrt(tf.reduce_sum(score * tf.math.reduce_euclidean_norm(elite_coord - mean_coord,axis=-1)**2,axis=0))
-        std = tf.expand_dims(std, axis=-1)
+        std=tf.sqrt(tf.reduce_sum(score * tf.math.reduce_euclidean_norm(elite_coord - mean_coord,axis=-1,keepdims=True)**2,axis=0))
         print("std shape",std.shape)
 
         return mean, std
