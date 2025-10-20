017e37a1c2cdfd3693a95614fd69072df87c1278
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-10-20 15:06:06 +0900

    TDMPC

--- PR2/Worker.py
+++ PR2/Worker.py
@@ -378,7 +378,10 @@ class Worker():
                 (mean, tf.TensorShape([horizon, a_size]))
             ]
         )
-        for i in tf.range(iterations):
+
+        loop_iterations = int(iterations)
+
+        for i in tf.range(loop_iterations):
             samples_from_distribution=self.sample_from_distribution(mean,std) 
             allsamples=tf.concat([samples_from_actor,samples_from_distribution],axis=0)
             V=self.compute_return(allsamples,inits_for_return)
