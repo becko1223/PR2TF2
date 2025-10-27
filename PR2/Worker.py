7e94b0f4d36c0c3dbb2cb62db376684c9d368895
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-10-27 13:07:11 +0900

    TDMPC

--- PR2/Worker.py
+++ PR2/Worker.py
@@ -741,7 +741,7 @@ class Worker():
                     rnn_state=[rnn_state[0],rnn_state[1]]
 
 
-                    if(episode_count>3):
+                    if(episode_count>100):
                         #Let's MPPI
 
 
