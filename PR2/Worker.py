9d06e4288f5733193ca0943e0caceb0ae9946ef0
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-10-31 16:37:40 +0900

    TDMPC

--- PR2/Worker.py
+++ PR2/Worker.py
@@ -314,8 +314,8 @@ class Worker():
 
             penalty_tensor = tf.cond(condition, 
                 lambda: tf.cond(is_horizon_first_condition,
-                                lambda: compute_penalty(self.currEpisode, is_wait_action, -0.02)+compute_penalty(self.currEpisode,is_invalid_action,-0.2) , 
-                                lambda: compute_penalty(self.currEpisode, is_wait_action, -0.02)),
+                                lambda: compute_penalty(self.currEpisode, is_wait_action, -0.01)+compute_penalty(self.currEpisode,is_invalid_action,-0.2) , 
+                                lambda: compute_penalty(self.currEpisode, is_wait_action, -0.01)),
                 lambda: tf.zeros_like(rewards) 
             )
 
