3ab6356f1be7b68a1fb98f9207d5e86208846fc6
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-11-03 10:51:24 +0900

    TDMPC

--- PR2/parameters.py
+++ PR2/parameters.py
@@ -22,7 +22,7 @@ gammma_tdmpc            =0.99
 temperature             =0.25
 iterations              =1
 
-random_term             =2
+random_term             =300
 guide_term              =1500
 
 
