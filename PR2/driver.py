bd43af1526dd62f0dfc31895bc598a1ad9fc7516
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-11-14 18:40:41 +0900

    TDMPC

--- PR2/driver.py
+++ PR2/driver.py
@@ -209,6 +209,7 @@ def main():
         global_summary = tf.summary.create_file_writer(train_path)
         checkpoint = tf.train.Checkpoint(model=global_network, world_optimizer=world_optimizer,policy_optimizer=policy_optimizer)
 
+        print("os.path.exists :",os.path.exists(model_path))
         if not os.path.exists(model_path):
             checkpoint_manager=tf.train.CheckpointManager(checkpoint,"drive/MyDrive/MAPF/PR2TF2/PR2/"+model_path,1)
             print("checkpoint:", "./drive/MyDrive/MAPF/PR2TF2/PR2/"+model_path)
