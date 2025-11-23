175c5ddc533f69f1b01e0a96acca2e02d3f072e4
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-11-23 16:59:48 +0900

    TDMPC_ER

--- PR2/driver.py
+++ PR2/driver.py
@@ -427,7 +427,7 @@ class ReplayBuffer():
         obs = np.empty(( batch_size, horizon+1,11,11,11), dtype=np.float32)
         goals= np.empty((batch_size,horizon+1,3))
         actions = np.empty(( batch_size,horizon, ), dtype=np.float32)
-        rewards = np.empty((batch_size,horizon,  1), dtype=np.float32)
+        rewards = np.empty((batch_size,horizon,  ), dtype=np.float32)
         states = np.empty((batch_size,1,512),dtype=np.float32)
         valids = np.empty((batch_size,horizon,5),dtype=np.float32)
 
