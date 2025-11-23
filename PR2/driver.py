dc01921d45813fb32b61378093aa66461e39d3df
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-11-23 18:28:17 +0900

    TDMPC_ER

--- PR2/driver.py
+++ PR2/driver.py
@@ -578,7 +578,7 @@ def main():
                 for i in range(len(obsResults)):
                     replaybuffer.add(obsResults[i],goalsResults[i],actionsResults[i],rewardsResults[i],statesResults[i],validsResults[i])
                 if curr_episode>(random_term-2): #random_term個分が終わったタイミングから学習を始めたい。
-                    for i in range(NUM_THREADS*max_episode_length):
+                    for i in range(10):
                         obs,goals,actions,rewards,states,valids=replaybuffer.sample(batch_size,horizon)
                         loss_list=update(global_network,obs,goals,actions,rewards,states,valids,world_optimizer,policy_optimizer,curr_episode)
                         all_loss.append(loss_list)
