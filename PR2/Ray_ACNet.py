9eb4baeefa47858ad390a65c963ed86b892c528e
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-12-03 08:11:52 +0900

    TDMPC_ER_TS

--- PR2/Ray_ACNet.py
+++ PR2/Ray_ACNet.py
@@ -43,57 +43,57 @@ class ACRDNet(tf.keras.Model):
         w_init = tf.keras.initializers.VarianceScaling()
 
         #エンコード
-        self.encode_conv1=layers.TimeDistributed(layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu'))
-        self.encode_res1_conv1=layers.TimeDistributed(layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu'))
-        self.encode_res1_conv2=layers.TimeDistributed(layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation=None))
-        self.encode_layernorm1=layers.TimeDistributed(layers.LayerNormalization())
-        self.encode_res2_conv1=layers.TimeDistributed(layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu'))
-        self.encode_res2_conv2=layers.TimeDistributed(layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation=None))
-        self.encode_layernorm2=layers.TimeDistributed(layers.LayerNormalization())
+        self.encode_conv1=layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu')
+        self.encode_res1_conv1=layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu')
+        self.encode_res1_conv2=layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation=None)
+        self.encode_layernorm1=layers.LayerNormalization()
+        self.encode_res2_conv1=layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu')
+        self.encode_res2_conv2=layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation=None)
+        self.encode_layernorm2=layers.LayerNormalization()
         
 
 
 
         #状態遷移
-        self.dynamics_conv1=layers.TimeDistributed(layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu'))
-        self.dynamics_res1_conv1=layers.TimeDistributed(layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu'))
-        self.dynamics_res1_conv2=layers.TimeDistributed(layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation=None))
-        self.dynamics_layernorm1=layers.TimeDistributed(layers.LayerNormalization())
-        self.dynamics_res2_conv1=layers.TimeDistributed(layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu'))
-        self.dynamics_res2_conv2=layers.TimeDistributed(layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation=None))
-        self.dynamics_layernorm2=layers.TimeDistributed(layers.LayerNormalization())
+        self.dynamics_conv1=layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu')
+        self.dynamics_res1_conv1=layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu')
+        self.dynamics_res1_conv2=layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation=None)
+        self.dynamics_layernorm1=layers.LayerNormalization()
+        self.dynamics_res2_conv1=layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu')
+        self.dynamics_res2_conv2=layers.Conv2D(filters=FILTER,kernel_size=3,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation=None)
+        self.dynamics_layernorm2=layers.LayerNormalization()
 
                          
         #方策、価値、報酬
-        self.policy_conv1=layers.TimeDistributed(layers.Conv2D(filters=FILTER,kernel_size=1,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu'))  #flattenに際し、チャンネル数を減らす。
-        self.policy_layernorm1=layers.TimeDistributed(layers.LayerNormalization())
-        self.policy_flatten=layers.TimeDistributed(layers.Flatten())
+        self.policy_conv1=layers.Conv2D(filters=FILTER,kernel_size=1,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu')  #flattenに際し、チャンネル数を減らす。
+        self.policy_layernorm1=layers.LayerNormalization()
+        self.policy_flatten=layers.Flatten()
         self.policy_dense1=layers.Dense(units=256,kernel_initializer=tf.keras.initializers.Orthogonal(gain=1.0, seed=None),activation="elu")
         self.policy_layernorm2=layers.LayerNormalization()
         self.policy_dense2=layers.Dense(units=A_SIZE,kernel_initializer=NormalizedColumnsInitializer(1.0/float(A_SIZE)))
 
 
-        self.q1_conv1=layers.TimeDistributed(layers.Conv2D(filters=FILTER,kernel_size=1,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu'))
-        self.q1_layernorm1=layers.TimeDistributed(layers.LayerNormalization())
-        self.q1_flatten=layers.TimeDistributed(layers.Flatten())
+        self.q1_conv1=layers.Conv2D(filters=FILTER,kernel_size=1,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu')
+        self.q1_layernorm1=layers.LayerNormalization()
+        self.q1_flatten=layers.Flatten()
         self.q1_dense1=layers.Dense(units=256,kernel_initializer=tf.keras.initializers.Orthogonal(gain=1.0, seed=None),activation="elu")
         self.q1_layernorm2=layers.LayerNormalization()
         self.q1_dense2=layers.Dense(units=1,kernel_initializer=NormalizedColumnsInitializer(1.0/float(A_SIZE)))
 
 
         
-        self.q2_conv1=layers.TimeDistributed(layers.Conv2D(filters=FILTER,kernel_size=1,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu'))
-        self.q2_layernorm1=layers.TimeDistributed(layers.LayerNormalization())
-        self.q2_flatten=layers.TimeDistributed(layers.Flatten())
+        self.q2_conv1=layers.Conv2D(filters=FILTER,kernel_size=1,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu')
+        self.q2_layernorm1=layers.LayerNormalization()
+        self.q2_flatten=layers.Flatten()
         self.q2_dense1=layers.Dense(units=256,kernel_initializer=tf.keras.initializers.Orthogonal(gain=1.0, seed=None),activation="elu")
         self.q2_layernorm2=layers.LayerNormalization()
         self.q2_dense2=layers.Dense(units=1,kernel_initializer=NormalizedColumnsInitializer(1.0/float(A_SIZE)))
 
        
         
-        self.reward_conv1=layers.TimeDistributed(layers.Conv2D(filters=FILTER,kernel_size=1,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu'))
-        self.reward_layernorm1=layers.TimeDistributed(layers.LayerNormalization())
-        self.reward_flatten=layers.TimeDistributed(layers.Flatten())
+        self.reward_conv1=layers.Conv2D(filters=FILTER,kernel_size=1,strides=1,padding="same",data_format="channels_last",kernel_initializer=w_init, activation='relu')
+        self.reward_layernorm1=layers.LayerNormalization()
+        self.reward_flatten=layers.Flatten()
         self.reward_dense1=layers.Dense(units=256,kernel_initializer=tf.keras.initializers.Orthogonal(gain=1.0, seed=None),activation="elu")
         self.reward_layernorm2=layers.LayerNormalization()
         self.reward_dense2=layers.Dense(units=1,kernel_initializer=NormalizedColumnsInitializer(1.0/float(A_SIZE)))
@@ -117,18 +117,18 @@ class ACRDNet(tf.keras.Model):
 
         x=tf.concat([x,y],axis=-1)
 
-        x=self.encode_conv1(x)
+        x=layers.TimeDistributed(self.encode_conv1)(x)
         skip=x
-        x=self.encode_res1_conv1(x)
-        x=self.encode_res1_conv2(x)
+        x=layers.TimeDistributed(self.encode_res1_conv1)(x)
+        x=layers.TimeDistributed(self.encode_res1_conv2)(x)
         x=x+skip
-        x=self.encode_layernorm1(x)
+        x=layers.TimeDistributed(self.encode_layernorm1)(x)
 
         skip=x
-        x=self.encode_res2_conv1(x)
-        x=self.encode_res2_conv2(x)
+        x=layers.TimeDistributed(self.encode_res2_conv1)(x)
+        x=layers.TimeDistributed(self.encode_res2_conv2)(x)
         x=x+skip
-        x=self.encode_layernorm2(x)
+        x=layers.TimeDistributed(self.encode_layernorm2)(x)
 
         
         return x
@@ -144,12 +144,12 @@ class ACRDNet(tf.keras.Model):
         y=tf.expand_dims(tf.expand_dims(y,2),2)
         y=tf.tile(y,[1,1,11,11,1])
         x=tf.concat([x,y],axis=-1)
-        x=self.dynamics_conv1(x)
-        x=self.dynamics_res1_conv1(x)
-        x=self.dynamics_res1_conv2(x)
+        x=layers.TimeDistributed(self.dynamics_conv1)(x)
+        x=layers.TimeDistributed(self.dynamics_res1_conv1)(x)
+        x=layers.TimeDistributed(self.dynamics_res1_conv2)(x)
         x=self.dynamics_layernorm1(x)
-        x=self.dynamics_res2_conv1(x)
-        x=self.dynamics_res2_conv2(x)
+        x=layers.TimeDistributed(self.dynamics_res2_conv1)(x)
+        x=layers.TimeDistributed(self.dynamics_res2_conv2)(x)
         x=self.dynamics_layernorm2(x)
         return x
     
@@ -163,9 +163,9 @@ class ACRDNet(tf.keras.Model):
         y=tf.expand_dims(tf.expand_dims(y,2),2)
         y=tf.tile(y,[1,1,11,11,1])
         x=tf.concat([x,y],axis=-1)
-        x=self.reward_conv1(x)
-        x=self.reward_layernorm1(x)
-        x =self.reward_flatten(x)
+        x=layers.TimeDistributed(self.reward_conv1)(x)
+        x=layers.TimeDistributed(self.reward_layernorm1)(x)
+        x = layers.TimeDistributed(self.reward_flatten)(x)
         x=self.reward_dense1(x)
         x=self.reward_layernorm2(x)
         x=self.reward_dense2(x)
@@ -176,9 +176,9 @@ class ACRDNet(tf.keras.Model):
         tf.TensorSpec(shape=[None, None,11,11, FILTER], dtype=tf.float32)
     ])
     def policy(self,latent):
-        x=self.policy_conv1(latent)
-        x=self.policy_layernorm1(x)
-        x =self.policy_flatten(x)
+        x=layers.TimeDistributed(self.policy_conv1)(latent)
+        x=layers.TimeDistributed(self.policy_layernorm1)(x)
+        x = layers.TimeDistributed(self.policy_flatten)(x)
         x=self.policy_dense1(x)
         x=self.policy_layernorm2(x)
         x=self.policy_dense2(x)
@@ -194,9 +194,9 @@ class ACRDNet(tf.keras.Model):
         y=tf.expand_dims(tf.expand_dims(y,2),2)
         y=tf.tile(y,[1,1,11,11,1])
         x=tf.concat([x,y],axis=-1)
-        x=self.q1_conv1(x)
-        x=self.q1_layernorm1(x)
-        x =self.q1_flatten(x)
+        x=layers.TimeDistributed(self.q1_conv1)(x)
+        x=layers.TimeDistributed(self.q1_layernorm1)(x)
+        x = layers.TimeDistributed(self.q1_flatten)(x)
         x=self.q1_dense1(x)
         x=self.q1_layernorm2(x)
         x=self.q1_dense2(x)
@@ -212,9 +212,9 @@ class ACRDNet(tf.keras.Model):
         y=tf.expand_dims(tf.expand_dims(y,2),2)
         y=tf.tile(y,[1,1,11,11,1])
         x=tf.concat([x,y],axis=-1)
-        x=self.q2_conv1(x)
-        x=self.q2_layernorm1(x)
-        x =self.q2_flatten(x)
+        x=layers.TimeDistributed(self.q2_conv1)(x)
+        x=layers.TimeDistributed(self.q2_layernorm1)(x)
+        x = layers.TimeDistributed(self.q2_flatten)(x)
         x=self.q2_dense1(x)
         x=self.q2_layernorm2(x)
         x=self.q2_dense2(x)
