b2ebae75d1cfed4fb21c7807a352f6ed8d800327
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-12-03 08:27:42 +0900

    TDMPC_ER_TS

--- PR2/driver.py
+++ PR2/driver.py
@@ -147,52 +147,9 @@ def apply_gradients(global_network, gradients, world_optimizer,policy_optimizer,
 
 
 @tf.function
-def tape_calc(global_network,batch_obs, batch_goals, batch_rewards, batch_actions,  batch_valids):
+def tape_calc(global_network,batch_obs, batch_goals, batch_rewards, batch_actions,  batch_valids, variables_for_actor, variables_except_for_actor):
 
-    variables_for_actor=global_network.policy_conv1.trainable_variables+global_network.policy_layernorm1.trainable_variables+global_network.policy_dense1.trainable_variables+global_network.policy_layernorm2.trainable_variables+global_network.policy_dense2.trainable_variables
-    actor_variable_names = set([v.name for v in variables_for_actor])
-    all_trainable_variables = global_network.trainable_variables
-    variables_except_for_actor = (
-            # エンコーダ
-            global_network.encode_conv1.trainable_variables +
-            global_network.encode_res1_conv1.trainable_variables +
-            global_network.encode_res1_conv2.trainable_variables +
-            global_network.encode_layernorm1.trainable_variables +
-            global_network.encode_res2_conv1.trainable_variables +
-            global_network.encode_res2_conv2.trainable_variables +
-            global_network.encode_layernorm2.trainable_variables +
-            
-            # ダイナミクス
-            global_network.dynamics_conv1.trainable_variables +
-            global_network.dynamics_res1_conv1.trainable_variables +
-            global_network.dynamics_res1_conv2.trainable_variables +
-            global_network.dynamics_layernorm1.trainable_variables +
-            global_network.dynamics_res2_conv1.trainable_variables +
-            global_network.dynamics_res2_conv2.trainable_variables +
-            global_network.dynamics_layernorm2.trainable_variables +
-
-            # Q1
-            global_network.q1_conv1.trainable_variables +
-            global_network.q1_layernorm1.trainable_variables +
-            global_network.q1_dense1.trainable_variables +
-            global_network.q1_layernorm2.trainable_variables +
-            global_network.q1_dense2.trainable_variables +
-
-            # Q2
-            global_network.q2_conv1.trainable_variables +
-            global_network.q2_layernorm1.trainable_variables +
-            global_network.q2_dense1.trainable_variables +
-            global_network.q2_layernorm2.trainable_variables +
-            global_network.q2_dense2.trainable_variables +
-
-            # 報酬
-            global_network.reward_conv1.trainable_variables +
-            global_network.reward_layernorm1.trainable_variables +
-            global_network.reward_dense1.trainable_variables +
-            global_network.reward_layernorm2.trainable_variables +
-            global_network.reward_dense2.trainable_variables
-        )
-
+    
     rhos=tf.convert_to_tensor([[rho**i for i in range(horizon)] for _ in range(batch_size)])
     
 
@@ -310,7 +267,7 @@ def tape_calc(global_network,batch_obs, batch_goals, batch_rewards, batch_action
 
 
 
-def update(global_network, obs,goals,actions,rewards,valids, world_optimizer,policy_optimizer, curr_episode):
+def update(global_network, obs,goals,actions,rewards,valids, world_optimizer,policy_optimizer, curr_episode,variables_for_actor, variables_except_for_actor):
 
     batch_obs = tf.convert_to_tensor(obs,dtype=tf.float32) 
     batch_goals = tf.convert_to_tensor(goals,dtype=tf.float32)
@@ -320,52 +277,10 @@ def update(global_network, obs,goals,actions,rewards,valids, world_optimizer,pol
     batch_valids=tf.convert_to_tensor(valids,dtype=tf.float32)
     
 
-    variables_for_actor=global_network.policy_conv1.trainable_variables+global_network.policy_layernorm1.trainable_variables+global_network.policy_dense1.trainable_variables+global_network.policy_layernorm2.trainable_variables+global_network.policy_dense2.trainable_variables
-    actor_variable_names = set([v.name for v in variables_for_actor])
-    all_trainable_variables = global_network.trainable_variables
-    variables_except_for_actor = (
-            # エンコーダ
-            global_network.encode_conv1.trainable_variables +
-            global_network.encode_res1_conv1.trainable_variables +
-            global_network.encode_res1_conv2.trainable_variables +
-            global_network.encode_layernorm1.trainable_variables +
-            global_network.encode_res2_conv1.trainable_variables +
-            global_network.encode_res2_conv2.trainable_variables +
-            global_network.encode_layernorm2.trainable_variables +
-            
-            # ダイナミクス
-            global_network.dynamics_conv1.trainable_variables +
-            global_network.dynamics_res1_conv1.trainable_variables +
-            global_network.dynamics_res1_conv2.trainable_variables +
-            global_network.dynamics_layernorm1.trainable_variables +
-            global_network.dynamics_res2_conv1.trainable_variables +
-            global_network.dynamics_res2_conv2.trainable_variables +
-            global_network.dynamics_layernorm2.trainable_variables +
-
-            # Q1
-            global_network.q1_conv1.trainable_variables +
-            global_network.q1_layernorm1.trainable_variables +
-            global_network.q1_dense1.trainable_variables +
-            global_network.q1_layernorm2.trainable_variables +
-            global_network.q1_dense2.trainable_variables +
-
-            # Q2
-            global_network.q2_conv1.trainable_variables +
-            global_network.q2_layernorm1.trainable_variables +
-            global_network.q2_dense1.trainable_variables +
-            global_network.q2_layernorm2.trainable_variables +
-            global_network.q2_dense2.trainable_variables +
-
-            # 報酬
-            global_network.reward_conv1.trainable_variables +
-            global_network.reward_layernorm1.trainable_variables +
-            global_network.reward_dense1.trainable_variables +
-            global_network.reward_layernorm2.trainable_variables +
-            global_network.reward_dense2.trainable_variables
-        )
+    
 
     
-    world_grads,policy_grads,loss_list=tape_calc(global_network,batch_obs, batch_goals, batch_rewards, batch_actions,  batch_valids)
+    world_grads,policy_grads,loss_list=tape_calc(global_network,batch_obs, batch_goals, batch_rewards, batch_actions,  batch_valids, variables_for_actor,variables_except_for_actor)
 
     var_norms = tf.linalg.global_norm(global_network.trainable_variables)
 
@@ -741,7 +656,7 @@ def main():
                 if curr_episode>(random_term-2): #random_term個分が終わったタイミングから学習を始めたい。
                     for i in range(NUM_THREADS*max_episode_length//4):
                         obs,goals,actions,rewards,valids=replaybuffer.sample(batch_size,horizon)
-                        loss_list=update(global_network,obs,goals,actions,rewards,valids,world_optimizer,policy_optimizer,curr_episode)
+                        loss_list=update(global_network,obs,goals,actions,rewards,valids,world_optimizer,policy_optimizer,curr_episode,variables_for_actor,variables_except_for_actor)
                         all_loss.append(loss_list)
                         print("update loop")
                     avg_loss=list(np.mean(np.array(all_loss), axis=0))
