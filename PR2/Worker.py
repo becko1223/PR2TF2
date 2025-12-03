992e4fa228046cc80652f732dcb3b462b8231b4e
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-12-03 09:20:09 +0900

    TDMPC_ER_TS

--- PR2/Worker.py
+++ PR2/Worker.py
@@ -160,7 +160,8 @@ class Worker():
             actions_onehot = tf.expand_dims(actions_onehot, axis=1) 
             
             # Predict Next Latent (B, 1, dim)
-            current_latent = self.local_ACRD.dynamics(current_latent, actions_onehot).set_shape([num_actor_traj, 1, 11, 11, FILTER_SIZE])
+            current_latent = self.local_ACRD.dynamics(current_latent, actions_onehot)
+            current_latent.set_shape([num_actor_traj, 1, 11, 11, FILTER_SIZE])
             
             # 結果をTensorArrayに書き込む (B, A_SIZE)
             actions_ta = actions_ta.write(t, tf.squeeze(actions_onehot, axis=1))
