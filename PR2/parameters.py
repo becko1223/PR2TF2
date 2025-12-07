c5a25a15e7aaf327dc8512b53e84f2a95eeabb94
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-12-07 16:37:07 +0900

    TDMPC_ER_TS_MHA

--- PR2/parameters.py
+++ PR2/parameters.py
@@ -22,7 +22,7 @@ gammma_tdmpc            =0.95
 temperature             =0.25
 iterations              =1
 
-random_term             =5
+random_term             =25
 
 
 # observer parameters
@@ -56,7 +56,7 @@ GIFS_FREQUENCY_RL       = 512
 OUTPUT_IL_GIFS          = False
 IL_GIF_PROB             = 0.
 
-replay_buffer_size      =20 #3000  #8で割った値が最大の過去利用量。だがだんだん1エピソードの長さが短くなるのでバッファの保有ステップ数が減ってく。
+replay_buffer_size      =3000  #8で割った値が最大の過去利用量。だがだんだん1エピソードの長さが短くなるのでバッファの保有ステップ数が減ってく。
 batch_size              =512
 
 
