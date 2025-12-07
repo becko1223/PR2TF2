01a090a489b2c05904fb7497f0042b50afde8be2
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-12-07 09:55:37 +0900

    TDMPC_ER_TS_MHA

--- PR2/Ray_ACNet.py
+++ PR2/Ray_ACNet.py
@@ -58,7 +58,7 @@ class ACRDNet(tf.keras.Model):
         self.comm_encode_action_flatten= layers.TimeDistributed(layers.Flatten())
         self.comm_encode_vector=layers.TimeDistributed(layers.Dense(FILTER, activation=None))
 
-        self.comm_mha=layers.TimeDistributed(layers.MultiHeadAttention(heads=4,key_dim=FILTER,dropout=0.1))
+        self.comm_mha=layers.TimeDistributed(layers.MultiHeadAttention(num_heads=4,key_dim=FILTER,dropout=0.1))
         self.comm_mha_layernorm=layers.LayerNormalization()
         self.comm_feedforward1=layers.Dense(units=FILTER*4,kernel_initializer=tf.keras.initializers.Orthogonal(gain=1.0, seed=None),activation="relu")
         self.comm_feedforward2=layers.Dense(units=FILTER,kernel_initializer=tf.keras.initializers.Orthogonal(gain=1.0, seed=None),activation="linear")
