568ea718e162d6a365dd79d143aef1301dc952e5
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-12-13 11:16:49 +0900

    TDMPC_ER_TS_MHA

--- PR2/Worker.py
+++ PR2/Worker.py
@@ -989,6 +989,11 @@ class Worker():
 
                     # Get observation,reward, valid actions for each agent 
                     s1 = joint_observations[self.metaAgentID][self.agentID]
+
+                    if(joint_rewards[self.metaAgentID][self.agentID]==5):
+                        if self.metaAgentID==0 and self.agentID==1:
+                            print("status:1")
+
                     if(joint_rewards[self.metaAgentID][self.agentID]==-2.3):
                         episode_collision_count+=1
                         if self.metaAgentID==0 and self.agentID==1:
