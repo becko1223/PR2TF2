c005a3b8108a1c4699fb870c994e6e89e0d2994d
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-12-14 15:32:44 +0900

    TDMPC_ER_TS_MHA

--- PR2/Worker.py
+++ PR2/Worker.py
@@ -834,7 +834,9 @@ class Worker():
                     goal=tf.expand_dims(s[1],0)
                     goal=tf.expand_dims(goal,0)
                     goal=tf.cast(goal,dtype=tf.float32)
-                    goal=tf.concat([goal,tf.cast(a_onehot,dtype=tf.float32)],-1)
+                    a_onehot=tf.cast(a_onehot,dtype=tf.float32)
+                    a_onehot.set_shape([1,1,5])
+                    goal=tf.concat([goal,a_onehot],-1)
 
 
                     first_latent=self.local_ACRD.encode(ob,goal)
