62980b82ec1fbd2130b677800b83e43ce51ae688
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-12-22 14:52:40 +0900

    TDMPC_ER_MHA

--- PR2/Runner.py
+++ PR2/Runner.py
@@ -267,7 +267,7 @@ class Runner(object):
                 jobResults, metrics, is_imitation = self.imitationLearningJob(episodeNumber)
 
             elif COMPUTE_TYPE == COMPUTE_OPTIONS.multiThreaded:
-                obsResults, goalsResults, actionsResults, rewardsResults,  validsResults,messagesResults,masksResults,tentativeResults, metrics, is_imitation = self.multiThreadedJob(episodeNumber, mean_finishes)
+                obsResults, goalsResults, actionsResults, rewardsResults,  validsResults,messagesResults,masksResults,tentativeResults,rnnstatesResults, metrics, is_imitation = self.multiThreadedJob(episodeNumber, mean_finishes)
 
             elif COMPUTE_TYPE == COMPUTE_OPTIONS.synchronous:
                 print("not implemented")
@@ -283,7 +283,7 @@ class Runner(object):
                 "is_imitation": is_imitation
             }
 
-            result= obsResults, goalsResults, actionsResults, rewardsResults, validsResults,messagesResults,masksResults,tentativeResults, metrics, info
+            result= obsResults, goalsResults, actionsResults, rewardsResults, validsResults,messagesResults,masksResults,tentativeResults,rnnstatesResults, metrics, info
             return result
         except Exception as e:
             import traceback
