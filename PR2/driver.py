2149fd5b7c3d2b0d276f3ed521dfae607c160e37
kirita1223 <s2211988@u.tsukuba.ac.jp>
2025-12-22 17:14:46 +0900

    TDMPC_ER_MHA

--- PR2/driver.py
+++ PR2/driver.py
@@ -614,7 +614,7 @@ class ReplayBuffer():
             messages[i]=np.stack(self.messages_buffer[self.indexlist[sample_ids[i]][0]-self.deletecount][self.indexlist[sample_ids[i]][1]:self.indexlist[sample_ids[i]][1]+horizon+1])
             masks[i]=np.stack(self.masks_buffer[self.indexlist[sample_ids[i]][0]-self.deletecount][self.indexlist[sample_ids[i]][1]:self.indexlist[sample_ids[i]][1]+horizon+1])
             tentatives[i]=np.stack(self.tentatives_buffer[self.indexlist[sample_ids[i]][0]-self.deletecount][self.indexlist[sample_ids[i]][1]:self.indexlist[sample_ids[i]][1]+horizon+1])
-            rnnstates[i]=np.stack(self.rnnstates_buffer[self.indexlist[sample_ids[i]][0]-self.deletecount][self.indexlist[sample_ids[i]][1]])
+            rnnstates[i][0]=np.stack(self.rnnstates_buffer[self.indexlist[sample_ids[i]][0]-self.deletecount][self.indexlist[sample_ids[i]][1]])
         
         return obs,goals,actions,rewards,valids,messages,masks,tentatives,rnnstates,sample_ids,weights
     
