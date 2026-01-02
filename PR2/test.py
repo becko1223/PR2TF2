2cd6fcec309cb6d7b9466d1d33fe252db997bf8b
kirita1223 <s2211988@u.tsukuba.ac.jp>
2026-01-02 20:35:09 +0900

    TDMPC_ER_MHA_NORNN

--- PR2/test.py
+++ PR2/test.py
@@ -131,9 +131,9 @@ def main():
       print("checkpoint name:",p)
 
 
-      MAP_SIZES = [80]
-      DENSITIES = [0.30]
-      AGENT_COUNTS = [4, 8, 16, 32, 64]
+      MAP_SIZES = [40]
+      DENSITIES = [0]
+      AGENT_COUNTS = [16,64]
       NUM_EPISODES = 50
       MAX_STEPS = 256
 
