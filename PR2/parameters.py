8778e6922d5e1e3d1c9dc13b64fdde2c2a9f5b79
kirita1223 <s2211988@u.tsukuba.ac.jp>
2026-01-02 14:44:15 +0900

    TDMPC_ER_MHA_NORNN

--- PR2/parameters.py
+++ PR2/parameters.py
@@ -42,7 +42,7 @@ NUM_IL_META_AGENTS      = 0
 
 IS_ONESHOT              = True
 
-NUM_THREADS             = 4 # int(multiprocessing.cpu_count() / (2 * NUM_META_AGENTS))
+NUM_THREADS             = 8 # int(multiprocessing.cpu_count() / (2 * NUM_META_AGENTS))
 NUM_BUFFERS             = 1  # NO EXPERIENCE REPLAY int(NUM_THREADS / 2)
 
 # training parameters
