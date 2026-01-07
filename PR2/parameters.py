d95b973e64eb4f66bc66dc744ec4f32c669e00c2
kirita1223 <s2211988@u.tsukuba.ac.jp>
2026-01-07 14:43:46 +0900

    TDMPC_ER_MHA_NORNN

--- PR2/parameters.py
+++ PR2/parameters.py
@@ -22,7 +22,7 @@ gammma_tdmpc            =0.99
 temperature             =0.5
 iterations              =2
 
-random_term             =25
+random_term             =2
 correction_rate         =0
 
 # observer parameters
